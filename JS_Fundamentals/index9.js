9fab1958de892f88ece8ba6634d66954fb36019e
JoeC73 <josephcornell40@gmail.com>
2023-10-21 18:00:48 -0400

    working on index9

---
+++ JS_Fundamentals/index9.js
@@ -0,0 +1,14 @@
+let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
+let moreSports = teamSports;
+moreSports.push('Football', 'Baseball, Tennis');
+console.log(moreSports)
+
+
+let dog1 = 'Bingo';
+let dog2 = dog1;
+dog2 = 'Scruffy'
+console.log(dog2)
+let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+let cat2 = cat1;
+cat1.name = 'Puffy';
+console.log(cat2)
