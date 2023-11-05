b1c45e082e63e29b7e7ad110a0c008cbd0667719
JoeC73 <josephcornell40@gmail.com>
2023-11-05 01:38:31 -0400

    Fixed all exercises

--- JS_Intermediate/index3.js
+++ JS_Intermediate/index3.js
@@ -1,15 +1,40 @@
-// const animals = ['Tiger', 'Giraffe']
-// animals.push('Gorilla', 'Bear')
-// animals.unshift('Parrot', 'Fish')
-
+// Original array
+const animals = ['Tiger', 'Giraffe']
 // console.log(animals)
 
+// a) Add 2 new values to the end
+animals.push('Gorilla', 'Bear')
+
+// b) Add 2 new values to the beginning
+animals.unshift('Parrot', 'Fish')
+
+// c) Sort the values alphabetically
+animals.sort()
+
+// d) Write a function replaceMiddleAnimal(newValue) that replaces the value in the
+// middle of the animals array with newValue
+
+function replaceMiddleAnimal(newValue) {
+    const middleAnimal = Math.floor(animals.length / 2);
+    animals[middleAnimal] = newValue 
+}
+replaceMiddleAnimal('Zebra')
+
+// e) Write a function findMatchingAnimals(beginsWith)
+
+function findMatchingAnimal(beginsWith) {
+    return animals.filter(animal => animal.toLowerCase().startsWith(beginsWith))
+}
+console.log(findMatchingAnimal("pa"))
+
+// Step 2. return a new array of all animals
+
+
+// Step 3. Returns with the "beginsWith" string
 
 
 
 
-const animals = ['Tiger', 'Giraffe', 'Gorilla', 'Bear', 'Parrot', 'Fish']
-animals.sort();
-animals.splice(0, 3, 'Monkey')
-animals.unshift('Bear', 'Fish')
-console.log(animals)
+
+
+
--- JS_Intermediate/index8.js
+++ JS_Intermediate/index8.js
@@ -1,27 +1,30 @@
-const phoneBookABC = new Map()
-phoneBookABC.set('Annabelle', '0412312343') 
-phoneBookABC.set('Barry', '0433221117')
-phoneBookABC.set('Caroline', '0455221182')
-phoneBookABC.delete('Caroline', '0455221182')
-phoneBookABC.set('Caroline', '0455221153')
+// 8. The following code creates a new Map object for storing names beginning with A, B, or C
+// with their phone numbers.
 
+const phoneBookABC = new Map();
+phoneBookABC.set("Annabelle", "0412312343");
+phoneBookABC.set("Barry", "0433221117");
 
+// c) Update the phone number for Caroline
+phoneBookABC.set("Caroline", "0455221182");
+phoneBookABC.delete("Caroline", "0455221182");
+phoneBookABC.set("Caroline", "0455221153");
 
-    
+// a) Create a new phoneBookDEF Map to store names beginning with D, E or F
+// b) Initialise the contents of phoneBookDEF by passing in an array of keys/values
 
-const phoneBookDEF = new Map()
-phoneBookDEF.set('David', '04210573987')
-phoneBookDEF.set('Edward', '0456387129')
-phoneBookDEF.set('Fred', '0468735193')
+const phoneBookDEF = new Map();
+phoneBookDEF.set("David", "04210573987");
+phoneBookDEF.set("Edward", "0456387129");
+phoneBookDEF.set("Fred", "0468735193");
 for (let item of phoneBookDEF) {
-console.log(item)
+  console.log(item);
 }
-
-
-// console.log(phoneBookDEF.get('David'));
-// console.log(phoneBookDEF.size);
-// console.log(phoneBookDEF.has('Fred'))
-// console.log(phoneBookABC.get('Caroline'))
-
-const phoneBook = new Map([...phoneBookABC, ...phoneBookDEF]);
-console.log(phoneBook)
+// d) Write a function printPhoneBook(contacts) that prints the names and phone
+// numbers in the given Map
+function phoneBook(contacts) {
+  contacts.forEach((phoneBook) => console.log(phoneBook));
+}
+// e) Combine the contents of the two individual Maps into a single phoneBook Map
+phoneBook = new Map([...phoneBookABC, ...phoneBookDEF]);
+console.log(phoneBook);
