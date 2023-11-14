22160b6a2c687a8de600e244f930d51556fa341d
JoeC73 <josephcornell40@gmail.com>
2023-11-14 15:38:24 -0500

    Added exercise 10

--- JS_Advanced/index10.js
+++ JS_Advanced/index10.js
@@ -1,17 +1,31 @@
-import fetch from "node-fetch";
-globalThis.fetch = fetch;
-function fetchURLData(url) {
-  let fetchPromise = fetch(url).then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      throw new Error(`Request failed with status ${response.status}`);
-    }
-  });
-  return fetchPromise;
-}
-fetchURLData("https://jsonplaceholder.typicode.com/todos/1")
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error.message));
+// // import fetch from "node-fetch";
+// // globalThis.fetch = fetch;
+//  function fetchURLData(url) {
+//   let fetchPromise = fetch(url).then((response) => {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error(`Request failed with status ${response.status}`);
+//     }
+//   });
+//   return fetchPromise;
+// }
+// fetchURLData("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error.message));
 
 // a) Write a new version of this function using async/await
+
+const fetchURLData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+  if (!response.ok) {
+    throw new Error("user data not found...");
+  }
+
+  const data = await response.json();
+
+  console.log(data);
+};
+
+fetchURLData();
--- JS_Advanced/index5.js
+++ JS_Advanced/index5.js
@@ -1,6 +1,46 @@
-// 5. The following car object has several properties and a method which uses them to print a
-// description. When calling the function normally this works as expected, but using it from
-// within setTimeout fails. Why?
+// // 5. The following car object has several properties and a method which uses them to print a
+// // description. When calling the function normally this works as expected, but using it from
+// // within setTimeout fails. Why?
+
+// // a) Fix the setTimeout call by wrapping the call to car.description() inside a
+// // function
+
+// let car = {
+//   make: "Porsche",
+//   model: "911",
+//   year: 1964,
+//   description() {
+//     console.log(`This car is a ${this.make} ${this.model} from ${this.year}`);
+//   },
+// };
+// car.description();
+
+// setTimeout(function () {
+//   car.description();
+// }, 2000);
+
+// // b) Change the year for the car by creating a clone of the original and overriding it
+
+// let clonedCar = Object.create(
+//       Object.getPrototypeOf(car),
+//       Object.getOwnPropertyDescriptors(car)
+//   )
+
+//   clonedCar.year = 1973;
+
+//   clonedCar.description()
+
+//   setTimeout(() => {
+//       clonedCar.description();
+//     }, 2000);
+
+// c) Does the delayed description() call use the original values or the new values from
+// b)? Why?
+
+//  Original values
+
+// d) Use bind to fix the description method so that it can be called from within
+// setTimeout without a wrapper function
 
 let car = {
   make: "Porsche",
@@ -10,25 +50,9 @@ let car = {
     console.log(`This car is a ${this.make} ${this.model} from ${this.year}`);
   },
 };
-const carClone = { ...car, year: 1973 };
-
 car.description();
 
-setTimeout(function () {
-  car.description();
-}, 200);
-
-// b) Change the year for the car by creating a clone of the original and overriding it
-// c) Does the delayed description() call use the original values or the new values from
-// b)? Why?
-//  Original values
-
-// d) Use bind to fix the description method so that it can be called from within
-// setTimeout without a wrapper function
-
-const boundDescription = car.description.bind(car);
-setTimeout(boundDescription, 200);
-const carClone1 = { ...car, model: 942 };
+setTimeout(car.description.bind(car), 2000);
 
 // e) Change another property of the car by creating a clone and overriding it, and test that
 // setTimeout still uses the bound value from d)
